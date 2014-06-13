b6cbbe8e1ef1ba83a021bff62f8b3f45c04a47a1
jpita <dvpita@gmail.com>
2014-06-13 14:09:47 +0100

    --first test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WatiN.Core;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread th = new Thread(new ThreadStart(Test));
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+            th.Join();
+            
+            
+        }
+        static void Test()
+        {
+            IE browser = null;
+            browser = new IE("http://www.google.com");
+            browser.TextField(Find.ByName("q")).TypeText("WatiN");
+            browser.Button(Find.ByName("btnG")).Click();
+            browser.Close();
+        }
+    }
+}
